f6a7d6f2fbe0e66b2ef2c5b6586d9044a58fbb54
Dan <danrousseau@protonmail.com>
2021-07-09 23:07:22 +0200

    Added base64 extension methods and tests.

--- src/Ardalis.Extensions/StringExtensions.cs
+++ src/Ardalis.Extensions/StringExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Ardalis.Extensions
 {
@@ -41,6 +43,54 @@ namespace Ardalis.Extensions
             }
 
             return int.Parse(input);
+        }
+
+
+        /// <summary>
+        /// Encodes string to base64.
+        /// 
+        /// Source: https://stackoverflow.com/questions/11743160/how-do-i-encode-and-decode-a-base64-string
+        /// </summary>
+        /// <param name="plainText">String to encode.</param>
+        /// <returns>The base64 encoded version of the string</returns>        
+        public static string Base64Encode(this string plainText)
+        {
+            if (string.IsNullOrEmpty(plainText))
+            {
+                return string.Empty;
+            }
+
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        /// <summary>
+        /// Decodes string from base64 to its normal representation.
+        /// 
+        /// Source: https://stackoverflow.com/questions/11743160/how-do-i-encode-and-decode-a-base64-string
+        /// Source: https://www.aspforums.net/Threads/567945/Check-if-Base64-string-is-Valid-or-not-using-C-and-VBNet-in-ASPNet/
+        /// </summary>
+        /// <param name="encodedString">String to encode.</param>
+        /// <returns>The decoded version of the string</returns>        
+        public static string DecodeBase64String(this string encodedString)
+        {
+            if (string.IsNullOrEmpty(encodedString))
+            {
+                return string.Empty;
+            }
+
+            // Check for valid base64
+            var base64 = encodedString.Trim();
+            var isValidBase64 = (encodedString.Length % 4 == 0) &&
+                                Regex.IsMatch(base64, @"^[a-zA-Z0-9\+/]*={0,3}$", RegexOptions.None);
+
+            if (!isValidBase64)
+            {
+                throw new ArgumentException($"{encodedString} is not valid base64");
+            }
+
+            var data = Convert.FromBase64String(encodedString);
+            return Encoding.UTF8.GetString(data);
         }
     }
 }
