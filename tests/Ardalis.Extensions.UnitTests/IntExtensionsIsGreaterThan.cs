de5fdbd529d05b0e972b9f332593b66d51de5280
MoatazAllam <mezo.allam2015@hotmail.com>
2021-07-10 04:49:54 +0200

    Adding InExtensiont to check if a number is greater than another number or not.

---
+++ tests/Ardalis.Extensions.UnitTests/IntExtensionsIsGreaterThan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace Ardalis.Extensions.UnitTests
+{
+    public class IntExtensionsIsGreaterThan
+    {
+        [Theory]
+        [InlineData(7)]
+        public void IsGreaterThan_GivenNumber_ShouldReturnsTrue(int number)
+        {
+            var result = number.IsGreaterThan(5);
+
+            Assert.True(result);
+        }                             
+    }
+}
