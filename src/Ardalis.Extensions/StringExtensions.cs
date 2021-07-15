8834d4e4ba1dbf93b9eb87c817e67f6cfc0b7e7a
Serega <seregaplyaskin8@gmail.com>
2021-07-15 12:28:07 +0300

    Add ReplaceString method

--- src/Ardalis.Extensions/StringExtensions.cs
+++ src/Ardalis.Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Ardalis.Extensions
 {
@@ -41,6 +42,34 @@ namespace Ardalis.Extensions
             }
 
             return int.Parse(input);
+        }
+
+        /// <summary>
+        /// Replaces old characters in a string with new characters a certain number of times.
+        /// </summary>
+        /// <param name="text">String to replace.</param>
+        /// <param name="oldString">Old string to be replaced.</param>
+        /// <param name="newString">New string to replace the old.</param>
+        /// <param name="count">Count of replacements.</param>
+        /// <returns>Obtain string with replaced characters.</returns>
+        public static string ReplaceString(this string text, string oldString, string newString, int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "Count can not be negative");
+            }
+            else if (oldString is null)
+            {
+                throw new ArgumentException(nameof(oldString), "OldString can not be null");
+            }
+            else if (newString is null)
+            {
+                throw new ArgumentException(nameof(newString), "NewString can not be null");
+            }
+
+            Regex regex = new Regex(Regex.Escape(oldString));
+
+            return regex.Replace(text, newString, count);
         }
     }
 }
---
+++ tests/Ardalis.Extensions.UnitTests/StringExtensionsReplaceString.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Ardalis.Extensions.UnitTests
+{
+    public class StringExtensionsReplaceString
+    {
+        [Theory]
+        [InlineData("Hello everyone!", "H.llo .v.ryone!", "e", ".", 3)]
+        [InlineData("sample sample text", "sample text", "sample ", "", 1)]
+        [InlineData("ABCABC", "AABCAABC", "A", "AA", 10)]
+        public void ReturnsChangedString(string input, string changedInput, string oldString, string newString, int count)
+        {
+            Assert.Equal(changedInput, input.ReplaceString(oldString, newString, count));
+        }
+    }
+}
