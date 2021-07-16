f8b933c7dd54dc4393448cd155cf49f3dc7f48ee
MoatazAllam <mezo.allam2015@hotmail.com>
2021-07-16 22:41:19 +0200

    Adding more tests for IsGreaterThan

--- tests/Ardalis.Extensions.UnitTests/IntExtensionsIsGreaterThan.cs
+++ tests/Ardalis.Extensions.UnitTests/IntExtensionsIsGreaterThan.cs
@@ -7,11 +7,29 @@ namespace Ardalis.Extensions.UnitTests
     {
         [Theory]
         [InlineData(7)]
-        public void IsGreaterThan_GivenNumber_ShouldReturnsTrue(int number)
+        public void IsGreaterThan_GivenNumberGreater_ShouldReturnsTrue(int number)
         {
             var result = number.IsGreaterThan(5);
 
             Assert.True(result);
-        }                             
+        }
+
+        [Theory]
+        [InlineData(3)]
+        public void IsGreaterThan_GivenNumberLess_ShouldReturnsFalse(int number)
+        {
+            var result = number.IsGreaterThan(4);
+
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        public void IsGreaterThan_GivenNumberEqual_ShouldReturnsFalse(int number)
+        {
+            var result = number.IsGreaterThan(2);
+
+            Assert.False(result);
+        }
     }
 }
