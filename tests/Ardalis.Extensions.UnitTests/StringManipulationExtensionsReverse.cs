fa67f48c589b715eb5c91f5cfa40ae6126a52961
Sebastian <sebastian.svobo@gmail.com>
2021-07-16 23:50:47 +0200

    Added string reverse extension

---
+++ tests/Ardalis.Extensions.UnitTests/StringManipulationExtensionsReverse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace Ardalis.Extensions.UnitTests
+{
+    public class StringManipulationExtensionsReverse
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void ReturnsEmptyStringGivenNullOrEmptyString(string input)
+        {
+            string result = input.Right(2);
+
+            Assert.Equal(String.Empty, result);
+        }
+
+        [Theory]
+        [InlineData("a", "a")]
+        [InlineData("abcde", "edcba")]
+        public void ReturnsReversedString(string input, string expectedResult)
+        {
+            string result = input.Reverse();
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
