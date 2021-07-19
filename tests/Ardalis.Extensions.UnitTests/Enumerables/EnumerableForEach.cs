d7e6b4985af71251026dee91d94dd472191ad480
Steve Smith <steve@kentsmiths.com>
2021-07-19 12:34:08 -0400

    Add enumerable foreach with tests

---
+++ tests/Ardalis.Extensions.UnitTests/Enumerables/EnumerableForEach.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xunit;
+using Ardalis.Extensions.Enumerables;
+using System.Collections.Generic;
+
+namespace Ardalis.Extensions.UnitTests.Enumerables
+{
+    public class EnumerableForEach
+    {
+        [Fact]
+        public void ThrowsGivenNullInput()
+        {
+            IEnumerable<object> enumeration = null;
+
+            Action action = () => enumeration.ForEach(x => x.ToString());
+
+            Assert.Throws<ArgumentNullException>(action);
+        }
+
+        [Fact]
+        public void ThrowsGivenNullAction()
+        {
+            IEnumerable<object> enumeration = new List<object>();
+
+            Action<object> enumAction = null;
+
+            Action action = () => enumeration.ForEach(enumAction);
+
+            Assert.Throws<ArgumentNullException>(action);
+        }
+
+        [Fact]
+        public void ExecutesActionOncePerItem()
+        {
+            IEnumerable<int> input = new List<int> { 1, 2, 3 };
+            var sum = 0;
+            Action<int> action = (int x) => sum += x;
+
+            input.ForEach(action);
+
+            Assert.Equal(6, sum);
+        }
+    }
+}
