cbd0393dba72e7ce18a2711a109566d4cc96f469
MoatazAllam <mezo.allam2015@hotmail.com>
2021-07-19 23:06:27 +0200

    Adding IntArrayExtensions and there tests

---
+++ src/Ardalis.Extensions/Arrays/IntArrayExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ardalis.Extensions.Arrays
+{
+    public static class IntArrayExtensions
+    {
+        public static int[] OddNumbers(this int[] input) =>
+            (input.Length != 0)
+            ? input.Where(i => (i % 2) != 0).OrderBy(i => i).ToArray()
+            : input;
+            
+
+        public static int[] EvenNumbers(this int[] input) =>
+            (input.Length != 0)
+            ? input.Where(i => (i % 2) == 0).OrderBy(i => i).ToArray()
+            : input;
+
+    }
+
+    
+}
---
+++ tests/Ardalis.Extensions.UnitTests/Arrays/IntArrayExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Ardalis.Extensions.Arrays;
+
+namespace Ardalis.Extensions.UnitTests.Arrays
+{
+    public class IntArrayExtensions
+    {
+        [Theory]
+        [InlineData(new int[0])]
+        public void ReturnsEmptyArrayGivenAnArray(int[] input)
+        {
+            var result = input.OddNumbers();
+
+            Assert.Empty(result);
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 5 })]
+        public void ReturnsOddNumbersGivenAnArray(int[] input)
+        {
+            var result = input.OddNumbers();
+
+            Assert.Equal(result, new[] { 1, 3, 5 });
+        }
+
+        [Theory]
+        [InlineData(new[] { 6, 7, 8, 9, 10, 11 })]
+        public void ReturnsEvenNumbersGivenAnArray(int[] input)
+        {
+            var result = input.EvenNumbers();
+
+            Assert.Equal(result, new[] { 6, 8, 10 });
+        }
+    }
+}
