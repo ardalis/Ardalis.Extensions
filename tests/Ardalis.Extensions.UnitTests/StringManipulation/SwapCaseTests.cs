6ce31601c9894327674c9954a64793145560ad27
ShadyNagy <info@shadynagy.com>
2021-07-23 21:14:24 +0200

    Add SwapCase for StringManipulation.

---
+++ tests/Ardalis.Extensions.UnitTests/StringManipulation/SwapCaseTests.cs
@@ -0,0 +1,33 @@
+﻿using Ardalis.Extensions.StringManipulation;
+using Xunit;
+
+namespace Ardalis.Extensions.UnitTests.StringManipulation
+{
+    public class SwapCaseTests
+    {
+        [Theory]
+        [InlineData("Hello everyone!", "hELLO EVERYONE!")]
+        [InlineData("HELLO", "hello")]
+        [InlineData("hello", "HELLO")]
+        public void ReturnsSwappedCaseString(string input, string sawppedCaseInput)
+        {
+            Assert.Equal(sawppedCaseInput, input.SwapCase());
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void ReturnsNotSwappedCaseStringIfNullOrEmpty(string input, string sawppedCaseInput)
+        {
+            Assert.Equal(sawppedCaseInput, input.SwapCase());
+        }
+
+        [Theory]
+        [InlineData("!@@#$#$%%^", "!@@#$#$%%^")]
+        [InlineData("\"'", "\"'")]
+        public void ReturnsNotSwappedCaseStringIfspecialCharacter(string input, string sawppedCaseInput)
+        {
+            Assert.Equal(sawppedCaseInput, input.SwapCase());
+        }
+    }
+}
