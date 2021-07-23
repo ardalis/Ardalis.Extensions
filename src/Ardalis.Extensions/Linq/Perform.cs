46e9204bf2fc4dc4288ef9b328fa597e88d580cf
Joe <josuave83@outlook.com>
2021-07-23 18:37:50 -0400

    Started Perform linq extension method

---
+++ src/Ardalis.Extensions/Linq/Perform.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ardalis.Extensions.Linq
+{
+    public static partial class LinqExtensionMethods
+    {
+        public static IEnumerable<T> Perform<T>(this IEnumerable<T> source, Func<T,T> func)
+        {
+
+        }
+    }
+}
