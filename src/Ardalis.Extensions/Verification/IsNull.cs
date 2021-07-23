2ae2bf295654e6b830991f9e3b10adeec325a0be
MoatazAllam <mezo.allam2015@hotmail.com>
2021-07-24 01:47:38 +0200

    Rename Checking to Verification folder

---
+++ src/Ardalis.Extensions/Verification/IsNull.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ardalis.Extensions.Verification
+{
+    public static partial class CheckingExtensionMethods
+    {
+        /// <summary>
+        /// Checks if a given object is null or not.
+        /// </summary>
+        /// <param name="value">value to check if it null or not.</param>
+        /// <returns>True if value is null otherwise false.</returns>
+        public static bool IsNull(this object value) =>
+             (value == null) ? true : false;
+    }
+}
---
+++ tests/Ardalis.Extensions.UnitTests/Verification/IsNullTests.cs
@@ -0,0 +1,29 @@
+﻿using Ardalis.Extensions.Verification;
+using System;
+using Xunit;
+
+namespace Ardalis.Extensions.UnitTests.Checking
+{
+    public class IsNullTests
+    {
+        [Theory]
+        [InlineData(null)]
+        public void ReturnsTrueGivenNull(object value)
+        {
+            var result = value.IsNull();
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("text")]
+        [InlineData(45)]
+        [InlineData(true)]
+        public void ReturnsFalseGivenValue(object value)
+        {
+            var result = value.IsNull();
+
+            Assert.False(result);
+        }
+    }
+}
