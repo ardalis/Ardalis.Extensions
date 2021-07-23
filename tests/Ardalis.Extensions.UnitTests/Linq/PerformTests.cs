dc91aed1ba9217385ba344d87b036bfd1af3be06
Joe <josuave83@outlook.com>
2021-07-23 18:52:16 -0400

    Added tests for Perform.

--- tests/Ardalis.Extensions.UnitTests/Linq/PerformTests.cs
+++ tests/Ardalis.Extensions.UnitTests/Linq/PerformTests.cs
@@ -1,6 +1,63 @@
-﻿namespace Ardalis.Extensions.UnitTests.Linq
+﻿using Ardalis.Extensions.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Ardalis.Extensions.UnitTests.Linq
 {
     public class PerformTests
     {
+        [Fact]
+        public void ThrowsGivenNullInput()
+        {
+            IEnumerable<object> enumeration = null;
+
+            Action action = () => enumeration.Perform(x => x.ToString()).ToList();
+
+            Assert.Throws<ArgumentNullException>(action);
+        }
+
+        [Fact]
+        public void ThrowsGivenNullAction()
+        {
+            IEnumerable<object> enumeration = new List<object>();
+
+            Func<object, object> operation = null;
+
+            Action action = () => enumeration.Perform(operation).ToList();
+
+            Assert.Throws<ArgumentNullException>(action);
+        }
+
+        [Fact]
+        public void ExecutesActionOncePerItem()
+        {
+            IEnumerable<int> input = new List<int> { 1, 2, 3 };
+            var sum = 0;
+            Func<int, int> operation = (int x) => sum += x;
+
+            input.Perform(operation).ToList();
+
+            Assert.Equal(6, sum);
+        }
+
+        [Fact]
+        public void ResultsCanBeUsedWithOtherLinqExpressions()
+        {
+            IEnumerable<int> input = new List<int> { 1, 2, 3 };
+
+            var sum = 0;
+            Func<int, int> operation = (int x) => sum += x;
+
+            var results = input.Perform(operation).Reverse().ToList();
+
+            var inspectors = new Action<int>[]{
+                (e) => Assert.Equal(6, e),
+                (e) => Assert.Equal(3, e),
+                (e) => Assert.Equal(1, e)};
+
+            Assert.Collection(results, inspectors);
+        }
     }
 }
