4b88775984ceb9efe15280b3ee0f5f63b4ff0613
MoatazAllam <mezo.allam2015@hotmail.com>
2021-07-24 02:41:19 +0200

    Add IsEmpty to Enumerable with tests

---
+++ src/Ardalis.Extensions/Enumerable/IsEmpty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ardalis.Extensions.Enumerable
+{
+    public static partial class EnumerableExtensionMethods
+    {
+        /// <summary>
+        /// Checks if a given enumerable is empty. 
+        /// </summary>
+        /// <typeparam name="T">The generic type of the enumerable.</typeparam>
+        /// <param name="input">True if enumerable is empty otherwise false </param>
+        /// <returns></returns>
+        public static bool IsEmpty<T>(this IEnumerable<T> input) =>
+            (input == null)
+            ? true
+            : false;
+    }
+}
---
+++ tests/Ardalis.Extensions.UnitTests/Enumerable/IsEmptyTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Ardalis.Extensions.Enumerable;
+using Xunit;
+
+namespace Ardalis.Extensions.UnitTests.Enumerable
+{
+    public class IsEmptyTests
+    {
+        [Fact]
+        public void ReturnsTrueGivenEmptyEnumerable()
+        {
+            IEnumerable<object> enumeration = null;
+
+            var result = enumeration.IsEmpty();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ReturnsFalseGivenEnumerable()
+        {
+            IEnumerable<int> enumeration = new List<int> { 1, 2, 3, 4 };
+
+            var result = enumeration.IsEmpty();
+
+            Assert.False(result);
+        }
+    }
+}
