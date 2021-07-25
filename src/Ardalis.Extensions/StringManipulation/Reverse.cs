b975fb1fc93429a393d257c52a117831ea0d645d
Sebastian <sebastian.svobo@gmail.com>
2021-07-25 16:35:26 +0200

    Safety update

--- src/Ardalis.Extensions/StringManipulation/Reverse.cs
+++ src/Ardalis.Extensions/StringManipulation/Reverse.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 namespace Ardalis.Extensions.StringManipulation
 {
@@ -16,32 +14,14 @@ namespace Ardalis.Extensions.StringManipulation
             if (string.IsNullOrEmpty(input)) return String.Empty;
             if (input.Length == 1) return input;
 
-            // Tradeoff: we can check whether the input is a palindrome (= same when reversed)
-            // to avoid extra allocation, but for the cost of extra time spent checking.
-            // Since most strings are not expected to be palindromes, we are simply not checking
-
-            // Alternative designs:
-            // 1. Get input char array (String.ToCharArray), reverse it (Array.Reverse) and use it in string ctor
-            // 2. Use StringBuilder and add characters in reverse order
-
-            string reversed = new string(' ', input.Length);
-
-            ref char target = ref MemoryMarshal.GetReference(reversed.AsSpan());
-            ref char targetEnd = ref Unsafe.Add(ref target, reversed.Length);
-
-            ref char source = ref MemoryMarshal.GetReference(input.AsSpan());
-            source = ref Unsafe.Add(ref source, input.Length - 1); // Set source to the last character
-
-            do
+            Span<char> result = input.Length > 1024 ? input.ToCharArray() : stackalloc char[input.Length];
+            if (input.Length <= 1024)
             {
-                target = source;
-
-                target = ref Unsafe.Add(ref target, 1);
-                source = ref Unsafe.Add(ref source, -1);
+                input.AsSpan().CopyTo(result);
             }
-            while (Unsafe.IsAddressLessThan(ref target, ref targetEnd));
+            result.Reverse();
 
-            return reversed;
+            return result.ToString();
         }
     }
 }
