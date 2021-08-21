52a9f52091da127b5c1da59b239ad86d370ff6b0
MoatazAllam <mezo.allam2015@hotmail.com>
2021-08-21 10:08:32 +0200

    Add IsAny to Enumerable with tests

---
+++ src/Ardalis.Extensions/Enumerable/IsAny.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Ardalis.Extensions.Enumerable
+{
+    public static partial class EnumerableExtensions
+    {
+        /// <summary>
+        /// Checks if a given enumerable is not empty. 
+        /// </summary>
+        /// <typeparam name="T">The generic type of the enumerable.</typeparam>
+        /// <param name="input">An IEnumerable<typeparamref name="T"/></param>
+        /// <returns>True if enumerable have at least one value otherwise false.</returns>
+        public static bool IsAny<T>(this IEnumerable<T> input) =>
+            input != null && input.Any();
+    }
+
+}
