ace9cba60e94127b7d34db6c91c92884441f6dbf
Steve Smith <steve@kentsmiths.com>
2021-10-22 14:25:35 -0400

    Add ReadUncommitted transaction extensions for use with EF/EF Core

---
+++ src/Ardalis.Extensions/Data/DataExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+
+namespace Ardalis.Extensions.Data
+{
+    public static partial class DataExtensions
+    {
+        // Source: https://stackoverflow.com/a/18553786
+        public static List<T> ToListReadUncommitted<T>(this IQueryable<T> query)
+        {
+            using (var scope = new TransactionScope(
+                TransactionScopeOption.Required,
+                new TransactionOptions()
+                {
+                    IsolationLevel = IsolationLevel.ReadUncommitted
+                }))
+            {
+                List<T> toReturn = query.ToList();
+                scope.Complete();
+                return toReturn;
+            }
+        }
+
+        // Source: https://stackoverflow.com/a/18553786
+        public static int CountReadUncommitted<T>(this IQueryable<T> query)
+        {
+            using (var scope = new TransactionScope(
+                TransactionScopeOption.Required,
+                new TransactionOptions()
+                {
+                    IsolationLevel = IsolationLevel.ReadUncommitted
+                }))
+            {
+                int toReturn = query.Count();
+                scope.Complete();
+                return toReturn;
+            }
+        }
+    }
+}
