9423d15fe45eace02b97225a3d86e2b836caede8
markus.gnigler <markus.gnigler@gmx.at>
2021-12-26 01:39:43 +0100

    Add concat many function

---
+++ src/Ardalis.Extensions/Linq/ConcatMany.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using Ardalis.GuardClauses;
+
+namespace Ardalis.Extensions.Linq
+{
+    public static partial class LinqExtensions
+    {
+
+        /// <summary>
+        /// Combines the given base iterator with all iterators in param parameter
+        /// </summary>
+        /// <typeparam name="TSource">The generic type of IEnumerable<typeparamref name="TSource"/></typeparam>
+        /// <param name="baseIterator">Base Iterator of IEnumerable<typeparamref name="TSource"/></param>
+        /// <param name="iterators">Iterators to be concatenated of IEnumerable<typeparamref name="TSource"/> Array</param>
+        /// <exception cref="System.ArgumentNullException">Throw if the baseIterator or the iterators to be concatenated are null.</exception>
+        /// <returns>Returns concatinated iterator.</returns>
+        public static IEnumerable<TSource> ConcatMany<TSource>(
+            this IEnumerable<TSource> baseIterator,
+            params IEnumerable<TSource>[] iterators)
+        {
+            Guard.Against.Null(baseIterator, nameof(baseIterator));
+            Guard.Against.Null(iterators, nameof(iterators));
+
+            var combined = iterators.Aggregate(
+                (IEnumerable<TSource> acc, IEnumerable<TSource> curr) => acc.Concat(curr));
+
+            return baseIterator.Concat(combined);
+        }
+    }
+}
