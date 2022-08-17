dc0158ef69a1ff1d626460803947dd8cac689815
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2022-08-17 12:07:09 -0400

    Optimize Lines(this string) with benchmarking

--- benchmarks/Ardalis.Extensions.Benchmarks/Program.cs
+++ benchmarks/Ardalis.Extensions.Benchmarks/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using BenchmarkDotNet.Running;
 
 namespace Ardalis.Extensions.Benchmarks
--- tests/Ardalis.Extensions.UnitTests/StringManipulation/LinesTests.cs
+++ tests/Ardalis.Extensions.UnitTests/StringManipulation/LinesTests.cs
@@ -2,13 +2,15 @@ using System.Linq;
 using Xunit;
 using Ardalis.Extensions.StringManipulation;
 
+namespace Ardalis.Extensions.UnitTests.StringManipulation;
+
 public class LinesTests
 {
   [Fact]
   public void SplitStringOnLineEndingLFWithoutFinalLineEnding()
   {
     var data = "\nMäry häd ä little lämb\n\nLittle lämb";
-    var lines = data.Lines().ToArray();
+    var lines = data.Lines().Select(line => line.ToString()).ToArray();
     Assert.Equal(new string[] {"", "Märy häd ä little lämb", "", "Little lämb"}, lines);
   }
 
@@ -16,7 +18,7 @@ public class LinesTests
   public void SplitStringOnLineEndingCRLFWithoutFinalLineEnding()
   {
     var data = "\r\nMäry häd ä little lämb\r\n\r\nLittle lämb";
-    var lines = data.Lines().ToArray();
+    var lines = data.Lines().Select(line => line.ToString()).ToArray();
     Assert.Equal(new string[] {"", "Märy häd ä little lämb", "", "Little lämb"}, lines);
   }
 
@@ -24,7 +26,7 @@ public class LinesTests
   public void SplitStringOnLineEndingWithFinalLineEnding()
   {
     var data = "\nMäry häd ä little lämb\n\r\nLittle lämb\n";
-    var lines = data.Lines().ToArray();
+    var lines = data.Lines().Select(line => line.ToString()).ToArray();
     Assert.Equal(new string[] {"", "Märy häd ä little lämb", "", "Little lämb"}, lines);
   }
 }
