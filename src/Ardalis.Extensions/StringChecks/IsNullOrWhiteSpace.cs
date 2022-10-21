c457c896eeb350ff52a7f096e8fcc1c885330160
Steve Smith <steve@kentsmiths.com>
2022-10-21 14:38:41 -0400

    Adding string checks and tests

---
+++ src/Ardalis.Extensions/StringChecks/IsNullOrWhiteSpace.cs
@@ -0,0 +1,11 @@
+﻿namespace Ardalis.Extensions.StringChecks;
+
+public static partial class StringCheckExtensions
+{
+  /// <summary>
+  /// Indicates whether the specified string is null, empty, or consists only of whitespace characters
+  /// </summary>
+  /// <param name="value">value to check whether null or empty or whitespace.</param>
+  /// <returns>True if value is null or empty or whitespace otherwise false.</returns>
+  public static bool IsNullOrWhiteSpace(this string value) => string.IsNullOrWhiteSpace(value);
+}
