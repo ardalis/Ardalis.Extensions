144ce6577872d686e18efdfeaa5d795c43f14dbf
Steve Smith <steve@kentsmiths.com>
2023-03-06 19:12:25 -0500

    add AddIfNotNull (needs tests)

--- src/Ardalis.Extensions/List/ListExtensions.cs
+++ src/Ardalis.Extensions/List/ListExtensions.cs
@@ -10,6 +10,22 @@ namespace Ardalis.Extensions.List;
 public static class ListExtensions
 {
   /// <summary>
+  /// Add object to <see cref="IList{T}"/> only when not null.
+  /// </summary>
+  /// <typeparam name="T">The type contained within the <see cref="IList{T}"/>.</typeparam>
+  /// <param name="obj">The <see cref="IList{T}"/>.</param>
+  /// <param name="item">The item to potentially add.</param>
+  /// <exception cref="ArgumentNullException">Thrown if obj is null</exception>
+  public static void AddIfNotNull<T>(this IList<T> obj, T item)
+  {
+    Guard.Against.Null(obj, nameof(obj));
+
+    if (item == null) return;
+
+    obj.Add(item);
+  }
+
+  /// <summary>
   /// Adds an <see cref="IEnumerable{T}"/> to an <see cref="IList{T}"/>.
   /// </summary>
   /// <typeparam name="T">The type contained within the <see cref="IList{T}"/>.</typeparam>
