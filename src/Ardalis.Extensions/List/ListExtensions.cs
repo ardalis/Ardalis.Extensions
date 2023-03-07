be947b28b440ea24e8dd1801eb015dd431a869ee
Steve Smith <steve@kentsmiths.com>
2023-03-06 19:11:10 -0500

    AddRangeIfNotNull with tests

---
+++ src/Ardalis.Extensions/List/ListExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Ardalis.GuardClauses;
+
+namespace Ardalis.Extensions.List;
+
+/// <summary>
+/// Source: https://github.com/Kritner-Blogs/ExtensionMethods
+/// </summary>
+public static class ListExtensions
+{
+  /// <summary>
+  /// Adds an <see cref="IEnumerable{T}"/> to an <see cref="IList{T}"/>.
+  /// </summary>
+  /// <typeparam name="T">The type contained within the <see cref="IList{T}"/>.</typeparam>
+  /// <param name="obj">The <see cref="IList{T}"/>.</param>
+  /// <param name="items">The items to potentially add.</param>
+  /// <exception cref="ArgumentNullException">Thrown if obj is null</exception>
+  public static void AddRangeIfNotNull<T>(this IList<T> obj, IEnumerable<T> items)
+  {
+    Guard.Against.Null(obj, nameof(obj));
+
+    if (items == null) return;
+
+    if (obj is List<T> objList)
+    {
+      objList.AddRange(items);
+      return;
+    }
+
+    foreach (var item in items)
+    {
+      obj.Add(item);
+    }
+  }
+}
---
+++ tests/Ardalis.Extensions.UnitTests/List/AddRangeIfNotNull.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ardalis.Extensions.Linq;
+using Ardalis.Extensions.List;
+using Xunit;
+
+namespace Ardalis.Extensions.UnitTests.Linq;
+
+public class AddRangeIfNotNull
+{
+  [Fact]
+  public void ThrowsGivenNullInput()
+  {
+    IList<object> list = null;
+
+    Action action = () => list.AddRangeIfNotNull(new List<object>());
+
+    Assert.Throws<ArgumentNullException>(action);
+  }
+
+  [Fact]
+  public void DoesNothingGivenNullItems()
+  {
+    var list = new List<int> { 1, 2, 3 };
+
+    list.AddRangeIfNotNull(null);
+
+    Assert.Equal(1, list.First());
+    Assert.Equal(3, list.Last());
+    Assert.Equal(3, list.Count);
+  }
+
+  [Fact]
+  public void DoesNothingGivenEmptyListOfItems()
+  {
+    var list = new List<int> { 1, 2, 3 };
+
+    list.AddRangeIfNotNull(new List<int>());
+
+    Assert.Equal(1, list.First());
+    Assert.Equal(3, list.Last());
+    Assert.Equal(3, list.Count);
+  }
+
+  [Fact]
+  public void AddsItemsToList()
+  {
+    var list = new List<int> { 1, 2, 3 };
+
+    list.AddRangeIfNotNull(new List<int>() { 4, 5 });
+
+
+    Assert.Equal(1, list.First());
+    Assert.Equal(5, list.Last());
+    Assert.Equal(5, list.Count);
+  }
+}
