e3d5f408513a1cb44c53a97f621d41842ad0d3d3
bsilvia <5374098+bsilvia@users.noreply.github.com>
2023-11-13 20:35:30 -0800

    Add XML docs

--- src/Ardalis.Extensions/Enumerable/RangeEnumerator.cs
+++ src/Ardalis.Extensions/Enumerable/RangeEnumerator.cs
@@ -4,16 +4,26 @@ namespace Ardalis.Extensions.Enumerable;
 
 public static partial class EnumerableExtensions
 {
-  public static RangeEnumerator GetEnumerator(this Range range)
-  {
-    return new RangeEnumerator(range);
-  }
+  /// <summary>
+  /// Defines an extension method to get a <see cref="RangeEnumerator"/> from a <see cref="Range"/>
+  /// that can be used in a foreach loop.
+  /// </summary>
+  /// <param name="range">Range over which to generate the enumerator</param>
+  /// <returns>Enumerator over the given range</returns>
+  public static RangeEnumerator GetEnumerator(this Range range) => new(range);
 
-  public static RangeEnumerator GetEnumerator(this int number)
-  {
-    return new RangeEnumerator(new Range(0, number));
-  }
+  /// <summary>
+  /// Defines an extension method to get a <see cref="RangeEnumerator"/> from an <see cref="int"/>
+  /// that can be used in a foreach loop.
+  /// </summary>
+  /// <param name="number">Ending number in the range</param>
+  /// <returns>Enumerator over a range from 0 to <paramref name="number"/></returns>
+  public static RangeEnumerator GetEnumerator(this int number) => new(new Range(0, number));
 
+  /// <summary>
+  /// Custom enumerator for a <see cref="Range"/> that can be used in a foreach loop.
+  /// This type is meant to reduce allocations and act like an <see cref="IEnumerator"/> via duck typing.
+  /// </summary>
   public ref struct RangeEnumerator
   {
     private int _current;
@@ -30,7 +40,7 @@ public static partial class EnumerableExtensions
       _end = range.End.Value;
     }
 
-    public int Current => _current;
+    public readonly int Current => _current;
 
     public bool MoveNext()
     {
