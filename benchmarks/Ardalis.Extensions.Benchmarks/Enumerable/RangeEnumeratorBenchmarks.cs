e1e200c4d79830945f263af1d910eb0150a51818
bsilvia <5374098+bsilvia@users.noreply.github.com>
2023-11-13 21:22:44 -0800

    Add benchmarks for RangeEnumerable

---
+++ benchmarks/Ardalis.Extensions.Benchmarks/Enumerable/RangeEnumeratorBenchmarks.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using Ardalis.Extensions.Enumerable;
+
+namespace Ardalis.Extensions.Benchmarks.Enumerable;
+
+[MemoryDiagnoser]
+[ReturnValueValidator(failOnError: true)]
+public class RangeEnumeratorBenchmarks
+{
+  [Params(10, 1000, 10000)]
+  public int End { get; set; }
+
+  [Benchmark(Baseline = true)]
+  public void NormalForLoop()
+  {
+    for (int i = 0; i < End; i++)
+    {
+      DoSomething(i);
+    }
+  }
+  
+  [Benchmark]
+  public void RangedForLoop()
+  {
+    foreach(var i in 0..End)
+    {
+      DoSomething(i);
+    }
+  }
+
+  // Prevent the compiler from optimizing away the loop
+  private static void DoSomething(int i) { }
+}
