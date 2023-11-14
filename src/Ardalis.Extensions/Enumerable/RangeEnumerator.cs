66ba946e5393de7ed788091dd1fca2910cdd3f5f
bsilvia <5374098+bsilvia@users.noreply.github.com>
2023-11-13 20:08:40 -0800

    Issue #60 - Add a range enumerator to support looping over ranges

---
+++ src/Ardalis.Extensions/Enumerable/RangeEnumerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ardalis.Extensions.Enumerable;
+
+public static partial class EnumerableExtensions
+{
+  public static RangeEnumerator GetEnumerator(this Range range)
+  {
+    return new RangeEnumerator(range);
+  }
+
+  public static RangeEnumerator GetEnumerator(this int number)
+  {
+    return new RangeEnumerator(new Range(0, number));
+  }
+
+  public ref struct RangeEnumerator
+  {
+    private int _current;
+    private readonly int _end;
+
+    public RangeEnumerator(Range range)
+    {
+      if (range.End.IsFromEnd)
+      {
+        throw new NotSupportedException();
+      }
+
+      _current = range.Start.Value - 1;
+      _end = range.End.Value;
+    }
+
+    public int Current => _current;
+
+    public bool MoveNext()
+    {
+      _current++;
+      return _current < _end;
+    }
+  }
+}
